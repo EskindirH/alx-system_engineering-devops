0037835b1b3a8a0cb0f4f50e05ad0a83810d8137
Eskindir H <eskindirhaileliul@gmail.com>
2024-06-04 06:17:00 -0700

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 """
-Script that queries the Reddit API and returns
-the number of subscribers for a given subreddit.
+Script that queries subscribers on a given Reddit subreddit.
 """
 
-
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
@@ -14,7 +11,8 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
+
+   if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
         return subscribers
