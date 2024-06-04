3fbec8db227a57432eef2ba988c569ac44a64d7a
Eskindir H <eskindirhaileliul@gmail.com>
2024-06-04 06:23:19 -0700

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and returns
-the number of subscribers for a given subreddit.
+Query that returns the number of subscribers.
 """
 import requests
 import sys
@@ -17,11 +16,16 @@ def number_of_subscribers(subreddit):
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     res = requests.get(url, headers=headers, allow_redirects=False)
+
     if res.status_code != 200:
         return 0
+
     dic = res.json()
+
     if 'data' not in dic:
         return 0
+
     if 'subscribers' not in dic.get('data'):
         return 0
+
     return res.json()['data']['subscribers']
