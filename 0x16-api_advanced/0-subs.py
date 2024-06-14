692236fcf4b9b9da0030c8857117787b9461e8e3
Eskindir H <eskindirhaileliul@gmail.com>
2024-06-14 01:16:56 -0700

    comment fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,7 +4,15 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers."""
+    """
+    Return the number of subscribers.
+    Args:
+        subreddit: subreddits.
+
+    Returns:
+        int: number of subscribers.
+    """
+
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "linx:0x16-api_advanced:v1.0.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
